0e0d7b9c15c799b01821394d2cc2a06eb3010670
Dom Vacchiano <djvacchiano@gmail.com>
2020-06-17 09:13:15 -0500

    Changed the form workflow so it redirects after the form submission and uses session to store and display the submitted data. More secure so can't reload and resubmit the form

--- dojoForm/views.py
+++ dojoForm/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        request.session['name'] = request.POST['name']
+        request.session['email'] = request.POST['email']
+        return redirect("/result")
+    else:
+        return render(request, "index.html")
 
 def result(request):
-    if request.method == 'POST':
-        print("a POST is being made to this route")
-        context = {
-            "name" : request.POST["name"],
-            "email" : request.POST["email"],
-        }
-        return render(request, "result.html", context)
-    else:
-        return redirect('')
+    # if request.method == 'POST':
+    #     print("a POST is being made to this route")
+    #     request.session['name'] = request.POST["name"]
+    #     request.session['email'] = request.POST["email"]
+    return render(request, "result.html")
+    # else:
+    #     return redirect('')
